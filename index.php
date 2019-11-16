72f44a36b9749f4d6028d23dc281af2ffd5cc981
IQWiksPell <stepan991220@gmail.com>
2019-11-16 13:49:05 +0300

    Лабораторная 3

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace alex_stepan;
+
+ini_set('display_errors', 1);
+error_reporting(-1);
+
+include_once('alex_stepan/Alex_Stepan_Exception.php');
+include_once('core/EquationInterface.php');
+include_once('core/LogAbstract.php');
+include_once('core/LogInterface.php');
+include_once('alex_stepan/Linear.php');
+include_once('alex_stepan/Square.php');
+include_once('alex_stepan/MyLog.php');
+
