73f00952b2134856b2cbd4b5168990f1e06397fb
IQWiksPell <stepan991220@gmail.com>
2019-11-16 13:56:20 +0300

    Лабораторная 4

--- index.php
+++ index.php
@@ -13,3 +13,35 @@ include_once('alex_stepan/Linear.php');
 include_once('alex_stepan/Square.php');
 include_once('alex_stepan/MyLog.php');
 
+
+echo "Please enter 3 parameters divided by space. \n";
+$params = explode(" ", fgets(STDIN));
+
+try {
+    if (count($params) != 3) {
+        throw new Alex_Stepan("Wrong parameter count. Please enter exactly 3 parameters.");
+    }
+    $a = (float)$params[0];
+    $b = (float)$params[1];
+    $c = (float)$params[2];
+    if ($params[0] == 0) {
+        MyLog::log("Linear equation: ".$b."x + ".$c." = 0");
+        $linear = new Linear();
+        MyLog::log("Answer: ".$linear->linear_equation($b, $c));
+    }
+    else {
+        MyLog::log("Square equation: ".$a."x^2 + ".$b."x + ".$c." = 0");
+        $square = new Square();
+        if (is_array($temp = $square->solve($a, $b, $c))) {
+            MyLog::log("Answer: ". implode(" , ", $temp));
+        }
+        else {
+            MyLog::log("Answer: ".$temp);
+        }
+    }
+}
+catch (Alex_Stepan $e){
+    MyLog::log($e->GetMessage());
+}
+
+MyLog::write()."\n";
